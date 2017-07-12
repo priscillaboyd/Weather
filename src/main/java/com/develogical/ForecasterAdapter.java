e2fa032e871d22a1afd2a1259fd9ba737ddadc87
blah <crcooper@outlook.com>
2017-07-12 15:37:27 +0100

    PB: Created adapter and interface with suitable tests.

---
+++ src/main/java/com/develogical/ForecasterAdapter.java
@@ -0,0 +1,26 @@
+package com.develogical;
+
+import com.weather.*;
+
+/**
+ * Created by ape03 on 12/07/2017.
+ */
+// should talk to the forecaster service
+public class ForecasterAdapter implements ForecasterInterface {
+
+    private Forecaster forecaster = null;
+
+    @Override
+    public String getOutlook(Region region, Day day) {
+        forecaster = new Forecaster();
+        Forecast forecast = forecaster.forecastFor(region, day);
+        return forecast.summary();
+    }
+
+    @Override
+    public int getTemperature(Region region, Day day) {
+        forecaster = new Forecaster();
+        Forecast forecast = forecaster.forecastFor(region, day);
+        return forecast.temperature();
+    }
+}
