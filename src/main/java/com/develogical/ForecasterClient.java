d26c07aec52a0dc55e4906cc1d70405c8bcce041
blah <crcooper@outlook.com>
2017-07-12 15:01:17 +0100

    NY: created test for temp and locations

--- src/main/java/com/develogical/ForecasterClient.java
+++ src/main/java/com/develogical/ForecasterClient.java
@@ -6,18 +6,35 @@ import com.weather.Forecaster;
 import com.weather.Region;
 
 public class ForecasterClient {
+
+    Forecaster forecaster = new Forecaster();
+
+    //get summary forecast
+    public String getOutlook(Region region, Day day) {
+        Forecast forecast = forecaster.forecastFor(region, day);
+        return forecast.summary();
+    }
+
+    public int getTemp(Region region, Day day) {
+        Forecast forecast = forecaster.forecastFor(region, day);
+        return forecast.temperature();
+    }
+
+
     public static void main(String[] args) {
         // This is just an example of using the 3rd party API - delete this class before submission.
-        Forecaster forecaster = new Forecaster();
 
-        Forecast londonForecast = forecaster.forecastFor(Region.LONDON, Day.MONDAY);
 
-        System.out.println("London outlook: " + londonForecast.summary());
-        System.out.println("London temperature: " + londonForecast.temperature());
-
-        Forecast edinburghForecast = forecaster.forecastFor(Region.EDINBURGH, Day.MONDAY);
-
-        System.out.println("Edinburgh outlook: " + edinburghForecast.summary());
-        System.out.println("Edinburgh temperature: " + edinburghForecast.temperature());
+//        Forecast londonForecast = forecaster.forecastFor(Region.LONDON, Day.MONDAY);
+//
+//        System.out.println("London outlook: " + londonForecast.summary());
+//        System.out.println("London temperature: " + londonForecast.temperature());
+//
+//        Forecast edinburghForecast = forecaster.forecastFor(Region.EDINBURGH, Day.MONDAY);
+//
+//        System.out.println("Edinburgh outlook: " + edinburghForecast.summary());
+//        System.out.println("Edinburgh temperature: " + edinburghForecast.temperature());
     }
+
+
 }
--- src/test/java/com/develogical/ForecasterClientTest.java
+++ src/test/java/com/develogical/ForecasterClientTest.java
@@ -8,11 +8,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class ForecasterClientTest {
+
+    Forecaster forecaster = new Forecaster();
     @Test
     public void getOutlookAndTemperatureForALocation() {
-        Forecaster forecaster = new Forecaster();
         Forecast londonForecast = forecaster.forecastFor(Region.LONDON, Day.MONDAY);
         assertNotNull(londonForecast.summary());
         assertNotNull(londonForecast.temperature());
+    };
+//    @Test
+//    public void getOutlookAndTemperatureForAnyLocation() {
+//        ForecasterClient forecastClient = new ForecasterClient();
+//        assertNotNull(forecastClient.getOutlook());
+//        assertNotNull(forecastClient.getTemp());
+//    };
+    @Test
+    public void getOutlookAndTemperatureForAnyLocation() {
+        ForecasterClient forecastClient = new ForecasterClient();
+        assertNotNull(forecastClient.getOutlook(Region.LONDON, Day.MONDAY));
+        assertNotNull(forecastClient.getTemp(Region.LONDON, Day.MONDAY));
     };
 }
