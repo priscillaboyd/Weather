5fc12158300a08090b9cb66468294bba553528fc
blah <crcooper@outlook.com>
2017-07-12 14:36:14 +0100

    PB: First commit. Added test to check that London forecast outlook and temperature can be retrieved

--- src/test/java/com/develogical/PutYourTestCodeInThisDirectoryTest.java
+++ src/test/java/com/develogical/PutYourTestCodeInThisDirectoryTest.java
@@ -1,13 +1,18 @@
 package com.develogical;
 
+import com.weather.*;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertNotNull;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class PutYourTestCodeInThisDirectoryTest {
     @Test
-    public void placeholder() throws Exception {
-        assertThat(2, equalTo(2));
-    }
+    public void getOutlookAndTemperatureForALocation() {
+        Forecaster forecaster = new Forecaster();
+        Forecast londonForecast = forecaster.forecastFor(Region.LONDON, Day.MONDAY);
+        assertNotNull(londonForecast.summary());
+        assertNotNull(londonForecast.temperature());
+    };
 }
